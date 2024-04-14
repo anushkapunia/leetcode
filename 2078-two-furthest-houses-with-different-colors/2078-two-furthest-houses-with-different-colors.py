d8dc50c364c70a5256b45bdcaca0ebaa57a179fd
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-04-14 20:34:09 +0530

    Time: 28 ms (37.66%), Space: 11.7 MB (14.29%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -1,6 +1,9 @@
 class Solution(object):
      def maxDistance(self, A):
-        i, j = 0, len(A) - 1
-        while A[0] == A[j]: j -= 1
-        while A[-1] == A[i]: i += 1
+        i=0
+        j =  len(A) - 1
+        while A[0] == A[j]: 
+            j -= 1
+        while A[-1] == A[i]: 
+            i += 1
         return max(len(A) - 1 - i, j)
