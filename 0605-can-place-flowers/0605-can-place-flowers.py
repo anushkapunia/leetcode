d34117b28e56fe8ab1ac90a538cd7031d532b006
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-04-17 12:22:37 +0530

    Time: 116 ms (67.22%), Space: 12.4 MB (38.33%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def canPlaceFlowers(self, fb, n):
-        count = 0
+        
         for i in range(len(fb)):
             if fb[i] == 0:
                 left = (i == 0) or (fb[i - 1] == 0)
                 right = (i == len(fb) - 1) or (fb[i + 1] == 0)
                 if left and right:
                     fb[i] = 1
-                    count+=1
+                    n = n-1
                 
                 
-        return count>=n
+        return n <= 0
