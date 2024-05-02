19d058fc5e6d7c5d4ef55fdea29d95c26ead561a
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-05-02 07:20:31 +0530

    Time: 28 ms (18.88%), Space: 11.5 MB (97.90%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,13 +1,13 @@
-class Solution(object):
+class Solution:
     def twoCitySchedCost(self, costs):
-        sorted_costs = sorted(costs, key=lambda x: x[0] - x[1])  # Sorting the costs based on the difference between aCost and bCost
-        n = len(costs) // 2  # Calculating n
-        min_cost = 0
-
-        for i in range(n):
-            min_cost += sorted_costs[i][0]  # Adding the cost of flying to city A for the first n people
-            min_cost += sorted_costs[i + n][1]  # Adding the cost of flying to city B for the remaining n people
-
-        return min_cost
-    
+        refund = []
+        N = len(costs)//2
+        minCost = 0
+        for A, B in costs:
+            refund.append(B - A)
+            minCost += A
+        refund.sort()
+        for i in range(N):
+            minCost += refund[i]
+        return minCost
         
