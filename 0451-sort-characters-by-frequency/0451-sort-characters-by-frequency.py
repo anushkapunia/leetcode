8660fb60379a4b0cb8182166f27442fb3d0b453a
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-20 08:44:44 +0530

    Time: 15 ms (99.30%), Space: 13.8 MB (98.08%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,16 +1,15 @@
-class Solution(object):
+class Solution:
     def frequencySort(self, s):
-        char_freq = Counter(s)
-        
-        # Create a list of buckets, where each bucket represents a frequency
-        buckets = [[] for _ in range(len(s) + 1)]
-        
-        # Populate the buckets with characters and their frequencies
-        for char, freq in char_freq.items():
-            buckets[freq].append(char * freq)
-        
-        # Build the answer string by iterating over the buckets in reverse order
-        answer = ''.join(char for bucket in reversed(buckets) for char in bucket)
-        
-        return answer
-        
+        # a string to store the final answer string
+        answerString = ''
+        # a dictionary that stores the frequency of each character
+        hashMap = defaultdict(int)
+        for i in s:
+            hashMap[i] = hashMap[i] + 1
+        # sorting the dictionary based on the values in descending order
+        sortedHashMap = sorted(hashMap.items(), key=lambda x: x[1], reverse=True)
+        # multiply the char with the frequency
+        for char, frequency in sortedHashMap:
+            answerString = answerString + char * frequency
+
+        return answerString
