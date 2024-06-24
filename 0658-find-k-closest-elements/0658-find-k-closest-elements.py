b588b8a159aab52e6d3f2608e5efbf85555709b5
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-24 10:47:14 +0530

    Time: 204 ms (92.69%), Space: 13.1 MB (80.57%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,5 +1,18 @@
 class Solution(object):
     def findClosestElements(self, arr, k, x):
+     
+        left = 0
+        right = len(arr) - k
+    
+        # Двоичный поиск для определения начальной позиции окна из k элементов
+        while left < right:
+            mid = left + (right - left) // 2
+            # Сравниваем расстояние от середины текущего окна до x
+            # с расстоянием от середины следующего (mid + k) окна до x
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1  # Сдвигаем левую границу вправо
+            else:
+                right = mid  # Сдвигаем правую границу влево
         
-        return sorted(heapq.nsmallest(k, arr, key=lambda i: abs(i-x+.1)))
-        
+        # Возвращаем k ближайших элементов, начиная с позиции left
+        return arr[left:left + k]
