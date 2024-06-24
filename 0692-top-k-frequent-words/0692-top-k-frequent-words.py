f9ed5124cfcef52c5b72657c18eb5fbefb3b9bc4
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-24 20:57:24 +0530

    Time: 34 ms (65.64%), Space: 10.9 MB (100.00%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,14 +1,7 @@
 class Solution(object):
     def topKFrequent(self, words, k):
- 
-        cnt=defaultdict(lambda :[0,""])
-        for i in words:
-            cnt[i]=[cnt[i][0]-1,i]
-        lst=list(cnt.values())
-        heapq.heapify(lst)
-        lst=heapq.nsmallest(k,lst)
-        ans=[]
-        for i in lst:
-            ans.append(i[1])
-        return ans
+        w = Counter(words)
         
+        s = sorted(w , key = lambda x : (-w[x] , x)) 
+        
+        return s[:k]
