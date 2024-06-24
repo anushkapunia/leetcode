2285b2ecd71d5fa6995730b11e7b8f2f51cc5a0e
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-24 12:14:28 +0530

    Time: 211 ms (78.25%), Space: 13.3 MB (56.51%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,21 +1,13 @@
 class Solution(object):
     def findClosestElements(self, arr, k, x):
-       
-        heap = []
-        
-        for num in arr:
-            diff = abs(num - x)
-            
-            # If the heap size is less than k, add the current element
-            if len(heap) < k:
-                heapq.heappush(heap, (-diff, -num))
+    
+        left = 0
+        right = len(arr) - 1
+    
+        while right - left >= k:
+            if abs(arr[left] - x) > abs(arr[right] - x):
+                left += 1
             else:
-                # If the current element is closer than the farthest element in the heap
-                if diff < -heap[0][0] or (diff == -heap[0][0] and num < -heap[0][1]):
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, (-diff, -num))
-        
-        # Extract the numbers from the heap and sort them
-        result = sorted(-num for _, num in heap)
-        return result
-        
+                right -= 1
+    
+        return arr[left:right+1]
