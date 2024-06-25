6c8a98e3d8a4e1eab4f049b56b34e5836b93a8c7
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-25 06:52:52 +0530

    Time: 471 ms (68.38%), Space: 25.5 MB (61.03%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,19 +1,11 @@
 class Solution(object):
     def minSetSize(self, arr):
-
-        c = Counter(arr)
-        h = [(-n, num) for num, n in c.items()]
-        heapq.heapify(h)
-        
-        total = 0
-        removed = 0
+   
         n = len(arr)
+        counts = sorted(Counter(arr).values(), reverse=True)
         
-        while removed < n // 2:
-            count, _ = heapq.heappop(h)
-            removed += -count
-            total += 1
-        
-        return total
-        
-        
+        removed = 0
+        for i, count in enumerate(counts, 1):
+            removed += count
+            if removed >= n // 2:
+                return i
