6881d76809e1d740d5d33422c77024f4ac875453
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-25 16:25:33 +0530

    Time: 735 ms (78.26%), Space: 23 MB (39.13%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,18 +1,22 @@
 class Solution(object):
     def maxResult(self, nums, k):
-     
-				n = len(nums)
-				D = [-float('inf')] * (n)
-				heap = [(-nums[0], 0)]
-				D[0] = nums[0]   
-				for i in range(1,n):
-					(maxValue, index) = heap[0]            
-					while index < i - k:
-						heapq.heappop(heap)
-						(maxValue, index) = heap[0]
 
-					D[i] = -maxValue + nums[i]
-					heapq.heappush(heap, (-D[i], i))
-
-				return D[n-1]
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        dq = deque([0])  # store indices
         
+        for i in range(1, n):
+            # remove indices outside the window
+            if dq[0] < i - k:
+                dq.popleft()
+            
+            dp[i] = nums[i] + dp[dq[0]]
+            
+            # remove indices with smaller values
+            while dq and dp[dq[-1]] <= dp[i]:
+                dq.pop()
+            
+            dq.append(i)
+        
+        return dp[n-1]
