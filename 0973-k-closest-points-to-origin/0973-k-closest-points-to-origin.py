46ff8a21f50666ebc9473fad8bb966e6d019a226
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-25 15:06:44 +0530

    Time: 545 ms (86.71%), Space: 17.8 MB (81.03%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,36 +1,12 @@
 class Solution(object):
     def kClosest(self, points, k):
-     
-        if not points or k <= 0:
-            return []
-        
-        def distance(point):
-            return point[0]**2 + point[1]**2
-        
-        def partition(left, right, pivot_idx):
-            pivot = distance(points[pivot_idx])
-            points[pivot_idx], points[right] = points[right], points[pivot_idx]
-            store_idx = left
-            for i in range(left, right):
-                if distance(points[i]) < pivot:
-                    points[store_idx], points[i] = points[i], points[store_idx]
-                    store_idx += 1
-            points[right], points[store_idx] = points[store_idx], points[right]
-            return store_idx
-        
-        def select(left, right):
-            if left == right:
-                return
-            
-            pivot_idx = random.randint(left, right)
-            pivot_idx = partition(left, right, pivot_idx)
-            
-            if k == pivot_idx:
-                return
-            elif k < pivot_idx:
-                select(left, pivot_idx - 1)
+ 
+        maxHeap = []
+        for id, p in enumerate(points):
+            d = p[0] * p[0] + p[1] * p[1]
+            if len(maxHeap) < k:
+                heappush(maxHeap, (-d, id))
             else:
-                select(pivot_idx + 1, right)
-        
-        select(0, len(points) - 1)
-        return points[:k]
+                heappushpop(maxHeap, (-d, id))
+
+        return [points[id] for _, id in maxHeap]  
