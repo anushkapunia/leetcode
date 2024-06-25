83dcda4cfdffea6c0a7dc0edc9bd5c716aa57fcf
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-25 15:05:33 +0530

    Time: 7519 ms (5.05%), Space: 25.1 MB (5.04%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,19 +1,36 @@
 class Solution(object):
     def kClosest(self, points, k):
-   
+     
         if not points or k <= 0:
             return []
         
-        h = []
-        for x, y in points:
-            dist = sqrt(x**2 + y**2)
-            heapq.heappush(h, (dist, (x, y)))
+        def distance(point):
+            return point[0]**2 + point[1]**2
         
-        r = []
-        while k > 0 :
-            s, p = heapq.heappop(h)
-            r.append(list(p))
-            k -= 1
+        def partition(left, right, pivot_idx):
+            pivot = distance(points[pivot_idx])
+            points[pivot_idx], points[right] = points[right], points[pivot_idx]
+            store_idx = left
+            for i in range(left, right):
+                if distance(points[i]) < pivot:
+                    points[store_idx], points[i] = points[i], points[store_idx]
+                    store_idx += 1
+            points[right], points[store_idx] = points[store_idx], points[right]
+            return store_idx
         
-        return r
+        def select(left, right):
+            if left == right:
+                return
+            
+            pivot_idx = random.randint(left, right)
+            pivot_idx = partition(left, right, pivot_idx)
+            
+            if k == pivot_idx:
+                return
+            elif k < pivot_idx:
+                select(left, pivot_idx - 1)
+            else:
+                select(pivot_idx + 1, right)
         
+        select(0, len(points) - 1)
+        return points[:k]
