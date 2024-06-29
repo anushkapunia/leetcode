044beca365694098f1ca7477a772ca4e25325564
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-06-29 08:11:10 +0530

    Time: 51 ms (81.14%), Space: 17.6 MB (42.76%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -5,28 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def __init__(self):
-        self.diff = list()
-    def inorderTraversal(self,root):
-        if root:
-            self.inorderTraversal(root.left)
-            self.diff.append(root.val)
-            self.inorderTraversal(root.right)
-
     def findTarget(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: bool
-        """
-        if (root == None):
-            return False
-        self.inorderTraversal(root)
-        n = len(self.diff)
-        for i in range(n):
-            for j in range(i+1,n):
-                if(self.diff[i]+self.diff[j]==k):
-                    return True
+  
+        def inorder(node):
+            if not node:
+                return False
+            
+            if k - node.val in seen:
+                return True
+            
+            seen.add(node.val)
+            
+            return inorder(node.left) or inorder(node.right)
         
-        
+        seen = set()
+        return inorder(root)
         
