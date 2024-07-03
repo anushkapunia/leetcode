b452ac1e09c779b89c6fefebfa12c96f46f1e0ff
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-07-03 06:59:29 +0530

    Time: 564 ms (97.92%), Space: 15.5 MB (22.37%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,36 @@
 class Solution(object):
     def threeSum(self, nums):
-  
-        nums.sort()  # Sort the array
-        result = []
-        n = len(nums)
-
-        for i in range(n - 2):
-            # Skip duplicates for i
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            
-            left = i + 1
-            right = n - 1
-
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-
-                if current_sum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    
-                    # Skip duplicates for left and right
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-                elif current_sum < 0:
-                    left += 1
-                else:
-                    right -= 1
-
-        return result
+     
+        res = set()
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0: 
+                n.append(num)
+            else:
+                z.append(num)
+        N, P = set(n), set(p)
+        
+        if z:
+            for num in P:
+                if -1*num in N:
+                    res.add((-1*num, 0, num))
+        
+        if len(z) >= 3:
+            res.add((0,0,0))
+        
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                target = -1*(n[i]+n[j])
+                if target in P:
+                    res.add(tuple(sorted([n[i],n[j],target])))
+        
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                target = -1*(p[i]+p[j])
+                if target in N:
+                    res.add(tuple(sorted([p[i],p[j],target])))
+        
+        return res
         
