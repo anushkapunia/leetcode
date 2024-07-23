a6c07c571d9f5badae8387f8aac3507756be2d6b
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-07-23 12:04:10 +0530

    Time: 118 ms (65.84%), Space: 12.7 MB (23.29%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,34 +1,36 @@
 class Solution(object):
     def findSubstring(self, s, words):
-    
-		length = len(words[0])
-		word_count = Counter(words)
-		indexes = []
-
-		for i in range(length):
-			start = i
-			window = defaultdict(int)
-			words_used = 0
-
-			for j in range(i, len(s) - length + 1, length):
-				word = s[j:j + length]
-
-				if word not in word_count:
-					start = j + length
-					window = defaultdict(int)
-					words_used = 0
-					continue
-
-				words_used += 1
-				window[word] += 1
-
-				while window[word] > word_count[word]:
-					window[s[start:start + length]] -= 1
-					start += length
-					words_used -= 1
-
-				if words_used == len(words):
-					indexes.append(start)
-
-		return indexes
+        index = []
+        w = Counter(words)
+        start = 0
+        l = len(words[0])
+        
+        
+        for i in range(l):
+            start = i
+            window = defaultdict(int)
+            wc = 0
+            
+            for j in range(i , len(s) - l+1 , l):
+                word = s[ j : j + l]
+                
+                if word not in words:
+                    start = j+l
+                    window = defaultdict(int)
+                    wc = 0
+                    continue
+                
+                window[word]+=1
+                wc+=1
+                
+                while window[word] > w[word]:
+                    window[s[start : start + l]] -=1
+                    start = start+ l
+                    wc-=1
+                    
+                if wc == len(words):
+                    index.append(start)
+                    
+        return index 
+                    
         
