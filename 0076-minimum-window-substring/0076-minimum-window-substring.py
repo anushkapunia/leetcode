b366c1c77bae5991a404faaeb8487e682006ba97
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-07-23 17:49:08 +0530

    Time: 202 ms (37.76%), Space: 12.3 MB (88.50%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,39 @@
 class Solution(object):
     def minWindow(self, s, t):
-      
+  
         if not s or not t:
             return ""
 
-        # Optimize the initial check
-        if len(t) == 1:
-            return t if t in s else ""
+        if t in s:
+            return t
 
-        target_counts = Counter(t)
-        window_counts = Counter()
-        required = len(target_counts)
+        letter_dict = {}
+        for letter in t:
+            letter_dict[letter] = letter_dict.get(letter, 0) + 1
+
+        required = len(letter_dict)
         formed = 0
+        window_counts = {}
         left = 0
         min_len = float('inf')
         result = ""
 
         for right, char in enumerate(s):
-            window_counts[char] += 1
-            
-            if char in target_counts and window_counts[char] == target_counts[char]:
-                formed += 1
+            if char in letter_dict:
+                window_counts[char] = window_counts.get(char, 0) + 1
+                if window_counts[char] == letter_dict[char]:
+                    formed += 1
 
             while formed == required:
                 if right - left + 1 < min_len:
                     min_len = right - left + 1
                     result = s[left:right+1]
 
-                window_counts[s[left]] -= 1
-                if s[left] in target_counts and window_counts[s[left]] < target_counts[s[left]]:
-                    formed -= 1
+                left_char = s[left]
+                if left_char in letter_dict:
+                    window_counts[left_char] -= 1
+                    if window_counts[left_char] < letter_dict[left_char]:
+                        formed -= 1
                 left += 1
 
         return result
