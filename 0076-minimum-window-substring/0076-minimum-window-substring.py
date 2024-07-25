10a8eb02db626399cb0deed5ba6d48662f754859
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-07-25 09:07:28 +0530

    Time: 69 ms (98.23%), Space: 15 MB (39.06%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -6,35 +6,46 @@ class Solution(object):
         l = defaultdict(int)
         
         for char in t:
-            l[char] += 1
-        
+                l[char]+=1
+                
         left = 0
-        rr, rl = 0, 0
-        cd = defaultdict(int)
+        rr , rl = 0 ,0
+        cd =defaultdict(int)
         c = 0
         minl = float('inf')
-        
+        cl = 0
+    
         for r in range(len(s)):
             w = s[r]
             
             if w in t:
-                cd[w] += 1
+                cd[w]+=1
                 if cd[w] == l[w]:
-                    c += 1
+                     c+=1
+                
+                
             
+                
             while c == len(l):
-                cl = r - left + 1
+                cl = r - left +1
                 
-                if cl < minl or minl == 0:
-                    minl = cl
-                    rr, rl = r, left
-                
-                if s[left] in t:
-                    cd[s[left]] -= 1
+                if cl < minl:
+                        minl = cl
+                        rr , rl = r, left
+                        
+                if s[left] in cd:
+                    cd[s[left]]-=1
                     if cd[s[left]] < l[s[left]]:
-                        c -= 1
+                         c-=1
+                    
+                left+=1
                 
-                left += 1
+        return s[rl:rr+1] if minl!=float('inf') else ""
+                
+                    
+               
+                
+                
+
         
-        return s[rl:rr+1] if minl != float('inf') else ""
         
