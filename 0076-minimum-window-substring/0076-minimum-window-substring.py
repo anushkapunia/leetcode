84b449cbe65f21835104bd29eed9b942bdec2a4a
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-07-25 09:05:34 +0530

    Time: 73 ms (97.39%), Space: 14.8 MB (43.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,54 +1,40 @@
 class Solution(object):
     def minWindow(self, s, t):
-     
-        letterDict = {}
-
         if t in s:
             return t
         
-        elif len(t) > len(s):
-            return ""
-
-        for letter in t:
-            if letter in letterDict:
-                letterDict[letter] += 1
-            else:
-                letterDict[letter] = 1
-
-        currLen =  0
-        minLen, n = float("inf"), len(s)
+        l = defaultdict(int)
+        
+        for char in t:
+            l[char] += 1
+        
         left = 0
-        currLetterDict = {}
-        required = len(letterDict)
-        retLeft, retRight = 0, 0
-        count = 0
-
-        for right in range(n):
-            letter = s[right]
-
-            if letter in letterDict:
-                if letter in currLetterDict:
-                    currLetterDict[letter] += 1
-                else:
-                    currLetterDict[letter] = 1
-
-                if currLetterDict[letter] == letterDict[letter]:
-                    count += 1
-
-            while count == required:
-                # valid substring
-                currLen = right - left + 1
-                # update res
-                if currLen < minLen:
-                    minLen = currLen
-                    retLeft, retRight = left, right
+        rr, rl = 0, 0
+        cd = defaultdict(int)
+        c = 0
+        minl = float('inf')
+        
+        for r in range(len(s)):
+            w = s[r]
+            
+            if w in t:
+                cd[w] += 1
+                if cd[w] == l[w]:
+                    c += 1
+            
+            while c == len(l):
+                cl = r - left + 1
                 
-                # update new curr letter letterDict
-                if s[left] in currLetterDict:
-                    currLetterDict[s[left]] -= 1
-                    if currLetterDict[s[left]] < letterDict[s[left]]:
-                        count -= 1
+                if cl < minl or minl == 0:
+                    minl = cl
+                    rr, rl = r, left
+                
+                if s[left] in t:
+                    cd[s[left]] -= 1
+                    if cd[s[left]] < l[s[left]]:
+                        c -= 1
+                
                 left += 1
-
-        return s[retLeft:retRight+1] if minLen != float("inf") else ""
+        
+        return s[rl:rr+1] if minl != float('inf') else ""
         
