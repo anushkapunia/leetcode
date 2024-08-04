97de9a209f0c6ad0bf630ebe11fb3c601d568a57
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-04 18:59:23 +0530

    Time: 715 ms (86.20%), Space: 20.9 MB (57.17%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        minp = float('inf')
+        maxp = 0
+        
+        for p in prices:
+            if p < minp:
+                minp = p
+            elif p - minp > maxp:
+                maxp = p - minp
+        
+        if maxp > 0:
+            return maxp
+        else:
+            return 0
+        
+        
