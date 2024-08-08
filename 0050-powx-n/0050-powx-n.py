023d5554e154f48ba1653fd14a56429211741483
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-08 07:49:29 +0530

    Time: 21 ms (11.04%), Space: 11.5 MB (68.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,25 +1,19 @@
 class Solution(object):
     def myPow(self, x, n):
- 
-        if n == 0:
-            return 1
-        if x == 0:
-            return 0
-    
-    # Handle negative exponents
+  
         if n < 0:
             x = 1 / x
             n = -n
-    
-        result = 1
-        while n > 0:
-        # If n is odd, multiply result by x
-            if n % 2 == 1:
-                result *= x
-        # Square x and halve n
-            x *= x
-            n //= 2
-    
-        return result
-    
+        
+        # Base case
+        if n == 0:
+            return 1.0
+        
+        # Recursive case
+        half = self.myPow(x, n // 2)
+        
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
         
