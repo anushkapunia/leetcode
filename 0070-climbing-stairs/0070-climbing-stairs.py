74cd1afa2250781eefa0f4429d78422b5b4119d0
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-09 07:23:02 +0530

    Time: 7 ms (94.58%), Space: 11.7 MB (28.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def climbStairs(self, n):
-        if n <=1:
+        
+        if n == 1:
             return 1
-        a , b = 1 , 1
-        
-        for _ in range(2,n+1):
-            a , b = b , a+b
-        
-        return b
+        a, b = 1, 2
+        for i in range(3, n + 1):
+            a,b = b ,a + b
             
-        
+        return b
