e13300edd1e8968b1e94a4c8d7667f0cc6ed428e
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-09 13:17:37 +0530

    Time: 419 ms (90.09%), Space: 11.6 MB (58.77%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,24 +1,40 @@
 class Solution(object):
     def threeSumClosest(self, nums, target):
-  
-        nums.sort()  # Sort the array
-        closest_sum = float('inf')  # Initialize with infinity
-
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
-
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-
-                # If the current sum is closer to the target than the closest sum
-                if abs(current_sum - target) < abs(closest_sum - target):
-                    closest_sum = current_sum
-
-                if current_sum < target:
-                    left += 1  # Move the left pointer to the right
-                elif current_sum > target:
-                    right -= 1  # Move the right pointer to the left
+        nums.sort()
+        r = sum(nums[:3])
+        fs = float('inf')
+        n = len(nums)
+        if r >= target:
+            return r
+        
+        for i in range(n-2):
+            if i>0 and nums[i] ==nums[i-1]:
+                continue
+                
+            l = i+1
+            r = n-1
+            
+            while l<r:
+                cs = nums[i] +nums[l] + nums[r]
+                if cs == target:
+                    return cs
+                
+                if abs(cs-target) < abs(fs-target):
+                    fs = cs
+                    
+                if cs < target:
+                    while l<r and nums[l] == nums[l+1]:
+                        l+=1
+                        
+                    l+=1
                 else:
-                    return current_sum  # If the current sum is exactly equal to the target
-
-        return closest_sum  # Return the closest sum found
+                    while l<r and nums[r] == nums[r-1]:
+                        r-=1
+                        
+                    r-=1
+                    
+        return fs
+                    
+                        
+    
+        
