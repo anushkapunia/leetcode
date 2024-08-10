51a7baafbccc3a3b55c9ea73061320eacebcc6da
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-10 07:05:24 +0530

    Time: 546 ms (87.33%), Space: 18.1 MB (70.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def kClosest(self, points, k):
+        if not points or k<=0:
+            return []
+        
         mh = []
         for i , p in enumerate(points):
             d = p[0]*p[0] + p[1]*p[1]
