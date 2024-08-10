37046669ab06934814b59cca8d1590adf5b4b330
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-10 07:04:04 +0530

    Time: 566 ms (73.34%), Space: 17.9 MB (83.87%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def kClosest(self, points, k):
- 
-        maxHeap = []
-        for id, p in enumerate(points):
-            d = p[0] * p[0] + p[1] * p[1]
-            if len(maxHeap) < k:
-                heappush(maxHeap, (-d, id))
+        mh = []
+        for i , p in enumerate(points):
+            d = p[0]*p[0] + p[1]*p[1]
+            if len(mh) <k:
+                heappush(mh , (-d,i))
             else:
-                heappushpop(maxHeap, (-d, id))
+                heappushpop(mh, (-d,i))
+                
+        return [points[i] for _ , i in mh]
 
-        return [points[id] for _, id in maxHeap]  
+                
+            
+        
+        
