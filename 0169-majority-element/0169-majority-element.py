779035edf7cc8a17d078ce97da5ca2fa3a90ab0d
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-13 17:58:21 +0530

    Time: 167 ms (34.83%), Space: 13.4 MB (37.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,10 @@
 class Solution(object):
     def majorityElement(self, nums):
-   
-        count = 0
-        candidate = None
-
+       
+        counts = {}
+        majority_count = len(nums) // 2
+        
         for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-        
-        return candidate
-
-        
+            counts[num] = counts.get(num, 0) + 1
+            if counts[num] > majority_count:
+                return num
