5d3725aecd304f7f6e9b5e1ac49a615b16efdff9
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-13 18:06:29 +0530

    Time: 154 ms (82.29%), Space: 13.4 MB (37.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,14 @@
 class Solution(object):
     def majorityElement(self, nums):
-        c = Counter(nums)
+     
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
         
-        return max(c.keys() , key = c.get)
-       
+        return candidate
+
         
