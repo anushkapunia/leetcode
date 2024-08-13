0161b2878343d23668ac39e48a860532814c2fce
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-13 17:38:31 +0530

    Time: 6054 ms (5.70%), Space: 13.3 MB (37.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,7 @@
 class Solution(object):
     def majorityElement(self, nums):
-        d = {}
-        for n in nums:
-            if n in d:
-                d[n] +=1
-            else:
-                d[n] = 0
-        h = []        
-        for a , b in d.items():
-            heapq.heappush(h , (-b , a))
-            
-        return h[0][1]
-            
+     
+        for i in nums:
+            if nums.count(i) > len(nums)//2:
+                return i
         
