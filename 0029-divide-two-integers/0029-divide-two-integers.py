e804b72fc07de3f90f85d04003ee3476811e0f0c
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-18 10:10:01 +0530

    Time: 12 ms (85.43%), Space: 11.6 MB (69.31%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def divide(self, dividend, divisor):
-    
-       
-        
+
         end = abs(dividend)
         d = abs(divisor)
         
@@ -10,11 +8,14 @@ class Solution(object):
         while end >= d:
             s = 0
             while end >= (d << s):
-                s += 1
+                s+=1
             r += 1 << (s-1)
-            end -= d << (s-1)
+            end-= d << (s-1)
+            
+        if (dividend < 0) != (divisor < 0) : r = -r
+            
+        return max(-2**31, min(2**31 - 1 , r))
+            
         
-        if (dividend < 0) != (divisor < 0):
-            r = -r
         
-        return max(-2**31, min(2**31 - 1, r))
+        
