d27747cf181e6f447f52b924de0bde416b605e5d
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-18 10:08:42 +0530

    Time: 21 ms (28.37%), Space: 11.4 MB (93.29%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def divide(self, dividend, divisor):
     
-        if dividend == -2**31 and divisor == -1:
-            return 2**31 - 1
+       
         
         end = abs(dividend)
         d = abs(divisor)
