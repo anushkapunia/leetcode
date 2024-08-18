f724cfc2e096998c0f6bd9488ace235bd497f52f
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-18 10:07:54 +0530

    Time: 12 ms (85.43%), Space: 11.7 MB (33.77%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,22 +1,21 @@
-class Solution:
+class Solution(object):
     def divide(self, dividend, divisor):
-        # Get absolute values
-        divid = abs(dividend)
-        div = abs(divisor)
+    
+        if dividend == -2**31 and divisor == -1:
+            return 2**31 - 1
         
-        result = 0
+        end = abs(dividend)
+        d = abs(divisor)
         
-        while divid >= div:
-            shift = 0
-            while divid >= (div << shift):
-                shift += 1
-            
-            result += (1 << (shift - 1))
-            divid -= div << (shift - 1)
+        r = 0
+        while end >= d:
+            s = 0
+            while end >= (d << s):
+                s += 1
+            r += 1 << (s-1)
+            end -= d << (s-1)
         
-        # Handle negative cases
-        if (dividend < 0) ^ (divisor < 0):
-            result = -result
+        if (dividend < 0) != (divisor < 0):
+            r = -r
         
-        # Handle overflow
-        return max(-2**31 ,min(2**31 -1 , result))
+        return max(-2**31, min(2**31 - 1, r))
