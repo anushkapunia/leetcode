f5fdf6cc2f4e90cd7e06601210fdaf011eb42eb9
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-08-24 16:41:05 +0530

    Time: 702 ms (69.25%), Space: 64.8 MB (92.99%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,22 +5,32 @@
 #         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
-        s = []
-        temp = head
+
+        if not head or not head.next:
+            return True
         
-        while temp != None:
-            s.append(temp.val)
-            temp = temp.next
-           
-        temp = head
-        for i in range(len(s)//2):
-            sv = s.pop()
-            if sv == temp.val:
-                temp = temp.next
-            else:
+        # Step 1: Find the middle of the linked list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse the second half of the list
+        prev = None
+        while slow:
+            next_node = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_node
+        
+        # Step 3: Compare the first half and the reversed second half
+        left, right = head, prev
+        while right:  # Compare till the end of the reversed second half
+            if left.val != right.val:
                 return False
-            
+            left = left.next
+            right = right.next
+        
         return True
 
-            
         
