5d23a9b4b4d8af92e9396c1432f12018aa11c164
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-09-06 21:53:53 +0530

    Time: 44 ms (69.32%), Space: 19.6 MB (77.99%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,29 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def deleteNode(self, r, k):
-        if not r:
-            return r
-        
-        if k < r.val:
-            r.left = self.deleteNode(r.left , k)
-        elif k > r.val:
-            r.right = self.deleteNode(r.right , k)
-        else:
-            if r.left is None:
-                return r.right
-            if r.right is None:
-                return r.left
-            
-            m = self.find(r.right )
-            r.val = m.val
-            r.right = self.deleteNode(r.right , m.val)
-            
-        return r
-
-            
     
-    def find(self , c):
-        while c.left:
-            c = c.left
-        return c
+    def findLastRight(self, root):
+        if root.right is None:
+            return root
+        return self.findLastRight(root.right)
+
+    # Helper function to delete the node
+    def helper(self, root):
+        if root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        rightChild = root.right
+        lastRight = self.findLastRight(root.left)
+        lastRight.right = rightChild
+        return root.left
+
+    # Function to delete a node in BST
+    def deleteNode(self, root, key):
+        if root is None:
+            return None
+        
+        # Traverse the tree to find the node to delete
+        if root.val == key:
+            return self.helper(root)
+        
+        dummy = root
+        while root:
+            if key < root.val:
+                if root.left and root.left.val == key:
+                    root.left = self.helper(root.left)
+                    break
+                else:
+                    root = root.left
+            else:
+                if root.right and root.right.val == key:
+                    root.right = self.helper(root.right)
+                    break
+                else:
+                    root = root.right
+
+        return dummy
