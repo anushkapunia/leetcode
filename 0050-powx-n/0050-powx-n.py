2898cc48e34e5ec184e6e6cd90f2dcf8f2268201
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-09-14 18:09:04 +0530

    Time: 12 ms (73.14%), Space: 11.5 MB (95.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -3,21 +3,17 @@ class Solution(object):
         if n < 0:
             x = 1/x
             n = -n
-
-        if n==0:
-            return 1
-        if n==1:
-            return x
         
-        h = self.myPow(x,n//2)
-        
-        if n%2 == 0:
-            return h *h
-        else:
-            return h*h*x
-        
-        
-  
-        
-        
+        r = 1
+        while n>0:
+            
+            if n % 2 == 0:
+                n = n//2
+                x*=x
+            else:
+                n = n-1
+                r = r*x
+            
+        return r   
+            
         
