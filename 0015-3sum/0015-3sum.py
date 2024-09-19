1a3af79631d7410e790359d0cb8e738c4de0117d
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-09-19 12:47:01 +0530

    Time: 902 ms (46.56%), Space: 17.2 MB (5.65%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,36 @@
 class Solution(object):
     def threeSum(self, nums):
-     
-        res = set()
-        n, p, z = [], [], []
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0: 
-                n.append(num)
-            else:
-                z.append(num)
-        N, P = set(n), set(p)
-        
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
-        
-        if len(z) >= 3:
-            res.add((0,0,0))
-        
-        for i in range(len(n)):
-            for j in range(i+1,len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-        
-        for i in range(len(p)):
-            for j in range(i+1,len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
-        
-        return res
+        nums.sort()
+        s = set()
+        for i in range(len(nums)-2):
+            n = nums[i]
+            
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            l = i+1
+            r = len(nums)-1
+            
+            while l<r:
+                v =  nums[i] + nums[l] +nums[r]
+                if v == 0:
+                    s.add((nums[i]  , nums[l] , nums[r]))
+                    
+                    while l <r and nums[r] == nums[r-1]:
+                        r-=1
+                      
+                    while l <r and nums[l] == nums[l+1]:
+                        l+=1
+                    l+=1
+                    r-=1
+                elif v > 0:
+                  
+                    r-=1
+                else:
+                   
+                    l+=1
+        return [list(x) for x in s]
+                    
+
+
         
