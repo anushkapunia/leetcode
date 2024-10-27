35219c25d0281dcd19ad4b2ca38bdab9d602e760
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-10-27 07:34:41 +0530

    Time: 23 ms (46.82%), Space: 11.6 MB (99.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,5 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-        a = list(t)
-        for i in range(len(s)):
-            if s[i] in a:
-                a.remove(s[i])
-            else:
-                return False
-            
-        return True
+        return Counter(s) == Counter(t)
+        
         
