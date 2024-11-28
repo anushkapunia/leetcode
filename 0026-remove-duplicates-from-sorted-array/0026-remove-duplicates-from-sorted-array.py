89751af589c813cd4acd9e9ca3652d16b891a40e
anushka <144930953+anushkapunia@users.noreply.github.com>
2024-11-28 18:40:04 +0530

    Time: 29 ms (18.11%), Space: 13 MB (63.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,16 +1,19 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        if not nums:
-            return 0  # Handle edge case for empty array
-
-    # Initialize the pointer for the position of the next unique element
-        unique_pos = 0
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[unique_pos]:  # Found a new unique element
-                unique_pos += 1  # Move the unique pointer
-                nums[unique_pos] = nums[i]  # Update the value at the unique pointer
-    
-        return unique_pos + 1  # The 
+        l = []
+        for n in nums:
+            if n in l:
+                continue
+            else:
+                l.append(n)
+                
+        for i in range(len(l)):
+            nums[i] = l[i]
+            
+        return len(l)
+            
+                
+        
+        
         
         
